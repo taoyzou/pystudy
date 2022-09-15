bf7f4005ad4f9a8a024815446882ea948f70e796
taoyzou <113102540+taoyzou@users.noreply.github.com>
2022-09-15 17:40:59 +0800

    Add files via upload
    陶勇州   2020144105 第一次作业

---
+++ wsd.py
@@ -0,0 +1,16 @@
+import turtle
+from turtle import *
+i=0
+num=6
+pendown()
+while i<num:
+      forward(100)
+      right(60)
+      i=i+1
+while i<12:
+      left(60)
+      forward(100)
+      right(120)
+      forward(100)
+      i=i+1
+done()
